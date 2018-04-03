73f4f53b8be05ef22ff8bf1ae53de40485d7af1e
Jane Weesner <jane.e.weesner@gmail.com>
2018-04-03 12:26:07 -0700

    Initital file setup complete. Static files served. post requests working with form input TODO: json parsing and csv response

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,18 @@
+var $ = jQuery;
+const URL = "http://127.0.0.1:3000/"
+$(document).ready(()=>{
+	$('.submit').on('click', function(e){
+		e.preventDefault(); //prevent console clear
+		postHandler($('.inputText').val());
+		$('.inputText').val('');
+	})
+})
 
-
+var postHandler= function(body){
+	fetch(URL, {
+		method: "POST",
+		//headers: {"content-type":"application/json"},
+		body: body
+	}).then((response)=>console.log(response.statusCode))
+	.catch(err => console.log(err));
+}
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,22 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var server = express();
+
+//server.use(bodyParser)
+
+server.listen(3000);
+console.log('Listening on 3000');
+server.use(express.static(__dirname + "/client"))
+server.use("/node_modules", express.static(__dirname+"/node_modules"))
+
+server.post('/', (req, res) => {
+	res.status(201);
+	res.set(headers)
+	res.send('cool');
+})
 
 
+var headers = {
+	"Access-Control-Allow-Origin": "*",
+	'Content-Type': 'text/plain'
+};

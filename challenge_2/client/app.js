06fdce5367b42e29d130d4bb5ce45ebefdaf8b4e
Jane Weesner <jane.e.weesner@gmail.com>
2018-04-03 17:55:49 -0700

    Add CSV parse and response, file loading capabilities

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,6 +1,12 @@
 var $ = jQuery;
 const URL = "http://127.0.0.1:3000/"
 $(document).ready(()=>{
+	$('.fileSelect').on('change', function(e){
+		reader = new FileReader();
+		reader.onload = (e) =>$('.inputText').val(e.target.result);
+		
+		reader.readAsText(this.files[0])
+	})
 	$('.submit').on('click', function(e){
 		e.preventDefault(); //prevent console clear
 		postHandler($('.inputText').val());
@@ -11,8 +17,10 @@ $(document).ready(()=>{
 var postHandler= function(body){
 	fetch(URL, {
 		method: "POST",
-		//headers: {"content-type":"application/json"},
-		body: body
-	}).then((response)=>console.log(response.statusCode))
+		headers: {"content-type":"application/json"},
+		body: JSON.stringify({info: body})
+	})
+	.then((response)=> response.text())
+	.then((resolve) => $('.csv').html(resolve.replace(/\n/g, "<br />")))
 	.catch(err => console.log(err));
 }
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -2,21 +2,57 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var server = express();
 
-//server.use(bodyParser)
+server.use(bodyParser.json());
 
 server.listen(3000);
 console.log('Listening on 3000');
 server.use(express.static(__dirname + "/client"))
 server.use("/node_modules", express.static(__dirname+"/node_modules"))
+//server.use(bodyParser.urlencoded())
+
+server.options('/', (req,res) =>{
+	res.set(headers);
+	res.send();
+})
 
 server.post('/', (req, res) => {
+	json = JSON.parse(req.body.info);
 	res.status(201);
-	res.set(headers)
-	res.send('cool');
+	res.set(headers);
+	res.send(toCSV(json));
 })
 
 
 var headers = {
 	"Access-Control-Allow-Origin": "*",
+	"Access-Control-Allow-Headers": "*",
 	'Content-Type': 'text/plain'
 };
+
+var toCSV= function(obj){
+	var indexCount = 0;
+	var fieldObj = {};
+	var csvArr = [];
+	var toDoObj = [obj]
+	while(toDoObj.length>0){
+		var currObj=toDoObj.pop();
+		csvArr.push([]);
+		for(var key in currObj){
+			if (key === "children"){
+				currObj.children.forEach((child)=>toDoObj.push(child));
+			} else if (fieldObj.hasOwnProperty(key)){
+				csvArr[csvArr.length-1][fieldObj[key]] = currObj[key];
+			}else{
+				fieldObj[key] = indexCount++;
+				csvArr[csvArr.length-1][fieldObj[key]] = currObj[key];
+			}
+		}
+	}
+	var finalStr = Object.keys(fieldObj) + '\n';
+	for (var i=0; i<csvArr.length; i++){
+		while (csvArr[i].length<indexCount){
+			csvArr[i].push("");
+		}
+	}
+	return finalStr + (csvArr.join('\n'));
+}

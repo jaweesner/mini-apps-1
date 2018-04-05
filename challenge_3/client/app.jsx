8696f538d67c1f16fc0b86e63e9c5c37ce735f21
Jane Weesner <jane.e.weesner@gmail.com>
2018-04-04 19:56:18 -0700

    Multiple players now allowed

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -2,18 +2,64 @@ class App extends React.Component{
 	constructor (props){
 		super(props);
 		this.state = {
-			message: "Let's Play!"
+			message: "Let's Play!",
+			win: false,
+			turn: 0,
+			players: ['purple', 'teal'],
+			potentialPlayers: ['grey','green','black']
 		}
 	}
 
 	onMessage(message){
-
 		this.setState({message: message||"Let's Play!"});
+	}
+
+	toggleWin(){
+		this.setState({win: !this.state.win});
+	}
+
+	incrementTurn(){
+		let newTurn = this.state.players.length-1 === this.state.turn ? 0 : ++this.state.turn;
+		this.setState({turn: newTurn});
+	}
+	reset(){
+		this.setState({
+			message: "Let's Play!",
+			win: false,
+			turn: 0,
+			players: ['purple', 'teal'],
+			potentialPlayers: ['grey','green','black']
+		})
+	}
+
+	playerChange(num){
+		if (num>0) {
+			if(this.state.potentialPlayers.length === 0){return}
+			this.state.players.push(this.state.potentialPlayers.pop())
+			this.setState({players:this.state.players});
+		}if(num<0){
+			if(this.state.players.length <=2){return}
+			this.state.potentialPlayers.push(this.state.players.pop());
+			this.setState({players:this.state.players})
+		} 
 	}
 
 	render(){
 		return <div>
-				<Board messageHandler={this.onMessage.bind(this)}/>
+				<Nav 
+					players = {this.state.players} 
+					turn = {this.state.turn} 
+					playerChange = {this.playerChange.bind(this)}
+					reset = {this.reset.bind(this)}
+				/>
+				<Board 
+					messageHandler={this.onMessage.bind(this)} 
+					win = {this.state.win} 
+					turn = {this.state.turn}
+					onWin = {this.toggleWin.bind(this)}
+					players = {this.state.players}
+					onTurn = {this.incrementTurn.bind(this)}
+				/>
 				<UserNotification message={this.state.message} />
 		</div>
 	}
@@ -25,36 +71,47 @@ class Board extends React.Component {
 	constructor (props){
 		super(props);
 		this.state = {
-			win: false,
-			turn: 'red',
-			board : [[],[],[],[],[],[],[]],
+			board : [],
+		}
+		while(this.state.board.length<props.players.length*4){
+			this.state.board.push([])
 		}
 	}
+
 	columnClickHandler(index){
-		if (this.state.win){ return } 
+		if (this.props.win){ return } 
 		var stackHeight = this.state.board[index].length
 
-		if (stackHeight===7) {
+		if (stackHeight===this.state.board.length) {
 			return this.props.messageHandler("That column is full. Choose another.")
 		}
 		this.props.messageHandler();
-		this.state.board[index].push(this.state.turn)
+		this.state.board[index].push(this.props.players[this.props.turn])
 		this.setState({board: this.state.board})
 		this.checkWins(index, stackHeight); //Pass in coords of the placed piece
-		this.setState({turn: this.state.turn === 'red' ? 'black' : 'red'})
+		this.checkTie();
+		this.props.onTurn();
 
 	}
 
+	checkTie(){
+		if(this.state.board.every((col) => col.length === this.state.board.length)){ 
+			this.props.messageHandler("Oh no, a tie...")
+			this.props.onWin();
+
+		}
+	}
+
 	checkWins(col, row){
-		var turn = this.state.turn;
-		var checkRow = function(rowVals){
+		var turnAlias = this.props.players[this.props.turn];
+		var checkRowDiag = function(rowVals){
 			var inARow = 1;
 			var rowIndex = col;
-			while(rowVals[--rowIndex] === turn){
+			while(rowVals[--rowIndex] === turnAlias){
 				inARow++;
 			}
 			rowIndex = col;
-			while(rowVals[++rowIndex] === turn){
+			while(rowVals[++rowIndex] === turnAlias){
 				inARow++;
 			}
 			return inARow>=4 ? true : false;
@@ -62,7 +119,7 @@ class Board extends React.Component {
 
 		var checkCol = function(colVals){
 			var inARow = 1;
-			while(colVals[--row] === turn){
+			while(colVals[--row] === turnAlias){
 				inARow++;
 			}
 			return inARow>=4 ? true : false;
@@ -75,35 +132,65 @@ class Board extends React.Component {
 		var minorArr = this.state.board.map(column => column[minorFirst--]||null);
 
 
-		if (checkRow(rowArr)||checkCol(this.state.board[col])||checkRow(majorArr)||checkRow(minorArr)){
-			this.props.messageHandler(turn + " wins!!!")
-			this.setState({win: true});
+		if (checkRowDiag(rowArr)||checkCol(this.state.board[col])||checkRowDiag(majorArr)||checkRowDiag(minorArr)){
+			this.props.messageHandler(turnAlias + " wins!!!")
+			this.props.onWin();
 		}
+	}
+	componentWillUpdate(){
+		if(this.state.board.length!==this.props.players.length*4){
+			this.state.board = [];
+			while(this.state.board.length<this.props.players.length*4){
+				this.state.board.push([])
+			}
+		}
+
 	}
 
 	render(){
 		return <div className= "gameBoard">
 			{this.state.board.map( (col, index) => 
-				<Column pieces={col} key = {index} index = {index} onClick = {this.columnClickHandler.bind(this)}/> )}
+				<Column 
+					pieces={col} 
+					key = {index} 
+					index = {index} 
+					onClick = {this.columnClickHandler.bind(this)} 
+					playerNum = {this.props.players.length} 
+				/> )}
 		</div>
 	}
 }
 
 var Column = (props) => (
 	<div className = "col" onClick = {(e)=>props.onClick(props.index)}>
-		{props.pieces.map((piece, index) => <Piece color = {piece} key = {index}/>)}
+		{props.pieces.map((piece, index) => <Piece color = {piece} key = {index} playerNum = {props.playerNum}/>)}
 	</div>
 	);
 
-var Piece = (props) => (
-	<div className = {"piece " + props.color}>
+var Piece = (props) => {
+	var style = {
+		"width" : 430/(props.playerNum*4),
+		"height": 430/(props.playerNum*4),
+		"backgroundColor": props.color
+
+	}
+	return <div style= {style} className = "piece"  >
 	</div>
-);
+};
 
 var UserNotification = (props) => (
-	<div className = "message">
+	<h1 className = "message">
 		{props.message}
-	</div>
+	</h1>
 );
 
+var Nav = (props) => (
+	<div>
+		<button onClick= {()=>props.playerChange(1)}>Add Player</button>
+		<button onClick= {()=>props.playerChange(-1)}>Remove Player</button>
+		<button onClick= {props.reset}>Reset Game</button>
+		<div> Players: {props.players.toString()} </div>
+		<div> {props.players[props.turn]}'s turn </div> 
+	</div>
+);
 ReactDOM.render(<App/>, document.getElementById('App'));
--- challenge_3/compiled/app.js
+++ challenge_3/compiled/app.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -17,24 +17,79 @@ var App = function (_React$Component) {
 		var _this = _possibleConstructorReturn(this, (App.__proto__ || Object.getPrototypeOf(App)).call(this, props));
 
 		_this.state = {
-			message: "Let's Play!"
+			message: "Let's Play!",
+			win: false,
+			turn: 0,
+			players: ['purple', 'teal'],
+			potentialPlayers: ['grey', 'green', 'black']
 		};
 		return _this;
 	}
 
 	_createClass(App, [{
-		key: "onMessage",
+		key: 'onMessage',
 		value: function onMessage(message) {
-
 			this.setState({ message: message || "Let's Play!" });
 		}
 	}, {
-		key: "render",
+		key: 'toggleWin',
+		value: function toggleWin() {
+			this.setState({ win: !this.state.win });
+		}
+	}, {
+		key: 'incrementTurn',
+		value: function incrementTurn() {
+			var newTurn = this.state.players.length - 1 === this.state.turn ? 0 : ++this.state.turn;
+			this.setState({ turn: newTurn });
+		}
+	}, {
+		key: 'reset',
+		value: function reset() {
+			this.setState({
+				message: "Let's Play!",
+				win: false,
+				turn: 0,
+				players: ['purple', 'teal'],
+				potentialPlayers: ['grey', 'green', 'black']
+			});
+		}
+	}, {
+		key: 'playerChange',
+		value: function playerChange(num) {
+			if (num > 0) {
+				if (this.state.potentialPlayers.length === 0) {
+					return;
+				}
+				this.state.players.push(this.state.potentialPlayers.pop());
+				this.setState({ players: this.state.players });
+			}if (num < 0) {
+				if (this.state.players.length <= 2) {
+					return;
+				}
+				this.state.potentialPlayers.push(this.state.players.pop());
+				this.setState({ players: this.state.players });
+			}
+		}
+	}, {
+		key: 'render',
 		value: function render() {
 			return React.createElement(
-				"div",
+				'div',
 				null,
-				React.createElement(Board, { messageHandler: this.onMessage.bind(this) }),
+				React.createElement(Nav, {
+					players: this.state.players,
+					turn: this.state.turn,
+					playerChange: this.playerChange.bind(this),
+					reset: this.reset.bind(this)
+				}),
+				React.createElement(Board, {
+					messageHandler: this.onMessage.bind(this),
+					win: this.state.win,
+					turn: this.state.turn,
+					onWin: this.toggleWin.bind(this),
+					players: this.state.players,
+					onTurn: this.incrementTurn.bind(this)
+				}),
 				React.createElement(UserNotification, { message: this.state.message })
 			);
 		}
@@ -55,42 +110,56 @@ var Board = function (_React$Component2) {
 		var _this2 = _possibleConstructorReturn(this, (Board.__proto__ || Object.getPrototypeOf(Board)).call(this, props));
 
 		_this2.state = {
-			win: false,
-			turn: 'red',
-			board: [[], [], [], [], [], [], []]
+			board: []
 		};
+		while (_this2.state.board.length < props.players.length * 4) {
+			_this2.state.board.push([]);
+		}
 		return _this2;
 	}
 
 	_createClass(Board, [{
-		key: "columnClickHandler",
+		key: 'columnClickHandler',
 		value: function columnClickHandler(index) {
-			if (this.state.win) {
+			if (this.props.win) {
 				return;
 			}
 			var stackHeight = this.state.board[index].length;
 
-			if (stackHeight === 7) {
+			if (stackHeight === this.state.board.length) {
 				return this.props.messageHandler("That column is full. Choose another.");
 			}
 			this.props.messageHandler();
-			this.state.board[index].push(this.state.turn);
+			this.state.board[index].push(this.props.players[this.props.turn]);
 			this.setState({ board: this.state.board });
 			this.checkWins(index, stackHeight); //Pass in coords of the placed piece
-			this.setState({ turn: this.state.turn === 'red' ? 'black' : 'red' });
+			this.checkTie();
+			this.props.onTurn();
 		}
 	}, {
-		key: "checkWins",
+		key: 'checkTie',
+		value: function checkTie() {
+			var _this3 = this;
+
+			if (this.state.board.every(function (col) {
+				return col.length === _this3.state.board.length;
+			})) {
+				this.props.messageHandler("Oh no, a tie...");
+				this.props.onWin();
+			}
+		}
+	}, {
+		key: 'checkWins',
 		value: function checkWins(col, row) {
-			var turn = this.state.turn;
-			var checkRow = function checkRow(rowVals) {
+			var turnAlias = this.props.players[this.props.turn];
+			var checkRowDiag = function checkRowDiag(rowVals) {
 				var inARow = 1;
 				var rowIndex = col;
-				while (rowVals[--rowIndex] === turn) {
+				while (rowVals[--rowIndex] === turnAlias) {
 					inARow++;
 				}
 				rowIndex = col;
-				while (rowVals[++rowIndex] === turn) {
+				while (rowVals[++rowIndex] === turnAlias) {
 					inARow++;
 				}
 				return inARow >= 4 ? true : false;
@@ -98,7 +167,7 @@ var Board = function (_React$Component2) {
 
 			var checkCol = function checkCol(colVals) {
 				var inARow = 1;
-				while (colVals[--row] === turn) {
+				while (colVals[--row] === turnAlias) {
 					inARow++;
 				}
 				return inARow >= 4 ? true : false;
@@ -116,21 +185,37 @@ var Board = function (_React$Component2) {
 				return column[minorFirst--] || null;
 			});
 
-			if (checkRow(rowArr) || checkCol(this.state.board[col]) || checkRow(majorArr) || checkRow(minorArr)) {
-				this.props.messageHandler(turn + " wins!!!");
-				this.setState({ win: true });
+			if (checkRowDiag(rowArr) || checkCol(this.state.board[col]) || checkRowDiag(majorArr) || checkRowDiag(minorArr)) {
+				this.props.messageHandler(turnAlias + " wins!!!");
+				this.props.onWin();
 			}
 		}
 	}, {
-		key: "render",
+		key: 'componentWillUpdate',
+		value: function componentWillUpdate() {
+			if (this.state.board.length !== this.props.players.length * 4) {
+				this.state.board = [];
+				while (this.state.board.length < this.props.players.length * 4) {
+					this.state.board.push([]);
+				}
+			}
+		}
+	}, {
+		key: 'render',
 		value: function render() {
-			var _this3 = this;
+			var _this4 = this;
 
 			return React.createElement(
-				"div",
-				{ className: "gameBoard" },
+				'div',
+				{ className: 'gameBoard' },
 				this.state.board.map(function (col, index) {
-					return React.createElement(Column, { pieces: col, key: index, index: index, onClick: _this3.columnClickHandler.bind(_this3) });
+					return React.createElement(Column, {
+						pieces: col,
+						key: index,
+						index: index,
+						onClick: _this4.columnClickHandler.bind(_this4),
+						playerNum: _this4.props.players.length
+					});
 				})
 			);
 		}
@@ -141,27 +226,72 @@ var Board = function (_React$Component2) {
 
 var Column = function Column(props) {
 	return React.createElement(
-		"div",
-		{ className: "col", onClick: function onClick(e) {
+		'div',
+		{ className: 'col', onClick: function onClick(e) {
 				return props.onClick(props.index);
 			} },
 		props.pieces.map(function (piece, index) {
-			return React.createElement(Piece, { color: piece, key: index });
+			return React.createElement(Piece, { color: piece, key: index, playerNum: props.playerNum });
 		})
 	);
 };
 
 var Piece = function Piece(props) {
-	return React.createElement("div", { className: "piece " + props.color });
+	var style = {
+		"width": 430 / (props.playerNum * 4),
+		"height": 430 / (props.playerNum * 4),
+		"backgroundColor": props.color
+
+	};
+	return React.createElement('div', { style: style, className: 'piece' });
 };
 
 var UserNotification = function UserNotification(props) {
 	return React.createElement(
-		"div",
-		{ className: "message" },
+		'h1',
+		{ className: 'message' },
 		props.message
 	);
 };
 
+var Nav = function Nav(props) {
+	return React.createElement(
+		'div',
+		null,
+		React.createElement(
+			'button',
+			{ onClick: function onClick() {
+					return props.playerChange(1);
+				} },
+			'Add Player'
+		),
+		React.createElement(
+			'button',
+			{ onClick: function onClick() {
+					return props.playerChange(-1);
+				} },
+			'Remove Player'
+		),
+		React.createElement(
+			'button',
+			{ onClick: props.reset },
+			'Reset Game'
+		),
+		React.createElement(
+			'div',
+			null,
+			' Players: ',
+			props.players.toString(),
+			' '
+		),
+		React.createElement(
+			'div',
+			null,
+			' ',
+			props.players[props.turn],
+			'\'s turn '
+		)
+	);
+};
 ReactDOM.render(React.createElement(App, null), document.getElementById('App'));
 //# sourceMappingURL=app.js.map
